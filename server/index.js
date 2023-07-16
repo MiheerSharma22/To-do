ffb48d2c1217da2b4ae608ba3fa846597cc67591
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-07-16 21:36:43 +0530

    added / in url in index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3000",
-  "https://to-do-frontend-psi.vercel.app",
+  "https://to-do-frontend-psi.vercel.app/",
 ];
 app.use(
   cors({
