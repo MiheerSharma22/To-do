9afa3238c7f8a875ec0de418d58de8843987c566
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-07-19 19:24:46 +0530

    removed URL of localhost(vanilla JS) from cors allowedOrigin

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(express.json());
 
 // adding cors middleware to facilitate communication between server(backend) and client(frontend) via requests and responses
 const allowedOrigins = [
-  "http://127.0.0.1:5500",
   "http://localhost:3000",
   "https://to-do-frontend-psi.vercel.app/",
 ];
