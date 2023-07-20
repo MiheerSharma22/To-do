8db3e70f42643399cf786d77a7a1338817633e93
MiheerSharma22 <miheer.sharma1@gmail.com>
2023-07-20 19:46:50 +0530

    changed origin

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
 ];
 app.use(
   cors({
-    origin: allowedOrigins, // front end path(url) from where the request will be made to the backend or server
+    origin: "https://to-do-frontend-psi.vercel.app", // front end path(url) from where the request will be made to the backend or server
     credentials: true,
   })
 );
